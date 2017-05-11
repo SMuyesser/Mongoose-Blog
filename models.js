ab6b9edd344da5e2246b100e14b49165556af0bb
SMuyesser <smuyesser@gmail.com>
2017-05-11 10:28:03 -0700

    Adds models.js and server with get method

---
+++ models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+    firstName: {type: String, required: true},
+    lastName: {type: String, require: true}
+  },
+  created: {type: String, required: true}
+});
+
+blogPostSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
+
+blogPostSchema.methods.apiRepr = function() {
+
+  return {
+    id: this._id,
+    title: this.title,
+    content: this.content,
+    author: this.authorName,
+    created: this.created
+  };
+}
+
+const blogPost = mongoose.model('blogPost', blogPostSchema);
+
+module.exports = {BlogPost};
